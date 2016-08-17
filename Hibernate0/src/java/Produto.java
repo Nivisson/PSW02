30ad7a874dfa0d27acf30e6edf5165209f03f11e
Nivisson <nivisson.lima@hotmail.com>
2016-08-16 21:18:43 -0300

    Hibernate0
    Hibernate0

---
+++ Hibernate0/src/java/Produto.java
@@ -0,0 +1,64 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ra21501998
+ */
+
+@Entity
+public class Produto {
+
+    
+    @Id
+    private String nome;
+    private String descricao;
+    private float preco;
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the preco
+     */
+    public float getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}
