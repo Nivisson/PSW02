d9c57140274c285546a182329dff8c0aa32ef4ca
Nivisson <nivisson.lima@hotmail.com>
2016-08-16 22:19:34 -0300

    Exercicio de Tela do projeto
    Tela de cadastro de peça e busca por ID

---
+++ ExercicioTelaProjetoi/src/java/Peca.java
@@ -0,0 +1,87 @@
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RA21501998
+ */
+@Entity
+public class Peca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+
+
+    private String nome;
+    private String km;
+    private String preco;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the km
+     */
+    public String getKm() {
+        return km;
+    }
+
+    /**
+     * @param km the km to set
+     */
+    public void setKm(String km) {
+        this.km = km;
+    }
+
+    /**
+     * @return the preco
+     */
+    public String getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+    
+}
